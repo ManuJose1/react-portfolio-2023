e8fe052fe50412f980bf35fc976b2e6160f067cf
ManuJose1 <n00200555@iadt.ie>
2023-10-25 21:50:16 +0100

    add pages and navbar

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { Browserrouter as Router, Routes, Route } from "react-router-dom";
+
+//Import Components
+import Navbar from "./components/Navbar";
+
+//Import Pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import NavExample from './pages/NavExample';
+import PageNotFound from './pages/PageNotFound'
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/nav" element={<NavExample />} />
+
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
+
+export default App;
